82fd921afdc6a030609326cc33de91df2ec5d829
Matt <Matthew.torres@vtsd.com>
2023-11-13 08:40:30 -0500

    hi

---
+++ 1-2-1/step 2.py
@@ -0,0 +1,25 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle as trtl
+wn = trtl.Screen()
+
+
+#-----game configuration----
+box_color = "black"
+
+
+#-----initialize turtle-----
+box = trtl.Turtle()
+box.color(box_color)
+box.shape("square")
+box.shapesize(4)
+box.fillcolor("blue")
+
+
+#-----game functions--------
+
+
+#-----events----------------
+
+trtl.Screen()
+wn.mainloop()
