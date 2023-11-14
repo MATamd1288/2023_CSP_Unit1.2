f49f069f542abb06cb0d783bc5737a70b38609a2
Matt <Matthew.torres@vtsd.com>
2023-11-14 07:54:36 -0500

    hi

--- 1-2-1/step 2.py
+++ 1-2-1/step 2.py
@@ -1,6 +1,7 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
+import random as rand
 wn = trtl.Screen()
 
 
@@ -17,9 +18,20 @@ box.fillcolor("blue")
 
 
 #-----game functions--------
+def box_clicked(x, y):
+    print("box was clicked!")
+    change_position()
+
+def change_position():
+    x = rand.randint(1,400)
+    y = rand.randint(1,300)
+    box.goto(x,y)
+    new_xpos = x
+    new_ypos = y
 
 
 #-----events----------------
-
+box.onclick(box_clicked)
 trtl.Screen()
 wn.mainloop()
+
