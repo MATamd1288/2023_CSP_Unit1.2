fca61406a2bb5943b2729f27a3ae0153bddaae97
Matt <Matthew.torres@vtsd.com>
2023-12-13 10:06:15 -0500

    Apple Avalanche Day 2

--- 1-2-3/apple_1.py
+++ 1-2-3/apple_1.py
@@ -9,6 +9,7 @@ wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
 
 apple = trtl.Turtle()
+drawer = trtl.Turtle()
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
@@ -16,17 +17,32 @@ def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
 
+def drawing():
+  drawer.color("red")
+  drawer.write("A", font = ("arial", 55, "bold"))
+
 def falling():
   apple.right(90)
   apple.penup()
   apple.forward(130)
   apple.pendown()
 
-wn.onkeypress(falling, "A")
+if wn.onkeypress(drawing, "a") == True:
+  falling()
+
+wn.listen()
+
+'''
+if wn.onkeypress(drawing, "A"):
+  falling()
+'''
+
+
 #-----function calls-----
 draw_apple(apple)
 wn.bgpic("background.gif")
 wn.mainloop()
+
 
 
 
