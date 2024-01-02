d76d0c3ec7e19975d815b28074a56b7ff9b6fb1f
Matt <Matthew.torres@vtsd.com>
2024-01-02 07:58:07 -0500

    maze

--- 1-2-4/a124_maze_doors_[MT].py
+++ 1-2-4/a124_maze_doors_[MT].py
@@ -4,9 +4,9 @@ maze_painter = trtl.Turtle()
 
 x = 20
 y = 20
-number = 28
 path_width = 10
 pencolor = ("black")
+
 
 maze_painter.speed(0)
 
@@ -20,10 +20,18 @@ for loop in range(29):
     maze_painter.forward(path_width * 2)
     maze_painter.pendown()
 
+#door
     maze_painter.left(90)
     maze_painter.forward(path_width * 4)
     maze_painter.back(path_width * 4)
     maze_painter.right(90)
+#barrier
+    maze_painter.forward(40)
+    maze_painter.right(90)
+    maze_painter.forward(path_width * 2)
+    maze_painter.back(path_width * 2)
+    maze_painter.left(90)
+
 
 
 
