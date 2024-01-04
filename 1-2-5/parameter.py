bf88ab5dd14aec0d4f7177e39779e87b24035596
Matt <Matthew.torres@vtsd.com>
2024-01-04 09:22:43 -0500

    maze

---
+++ 1-2-5/parameter.py
@@ -0,0 +1,8 @@
+import turtle as trtl
+
+example = trtl.Turtle()
+example.forward(40)
+example.right(90)
+
+distance = 50
+
---
+++ 1-2-5/parameter2.py
@@ -0,0 +1,13 @@
+x = 50
+y = 4
+z = 42
+
+def add_vals(num1, num2, num3):
+    final = x + y + z
+
+    if(num1 > num2):
+        print(num3)
+    else:
+        print("invalid imput")
+
+add_vals(3,4,7)
