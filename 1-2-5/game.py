2afcab4c22804b0ff61ce06d66ce82f9bda8b484
Matt <Matthew.torres@vtsd.com>
2024-01-09 07:53:19 -0500

    game

--- 1-2-5/game.py
+++ 1-2-5/game.py
@@ -1,27 +1,30 @@
 import turtle as trtl
 
-runner = trtl.Turtle
-
-
-def up():
-    if (wn.onkeypress("d")):
-        runner.forward(10)
-
-up()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 wn = trtl.Screen()
+runner = trtl.Turtle()
+
+runner.penup()
+
+
+
+#keypress
+def w():
+    runner.forward(20)
+
+def s():
+    runner.back(20)
+
+def a():
+    runner.left(45)
+
+def d():
+    runner.right(45)
+
+#listen for keypress
+wn.onkeypress(w, "w")
+wn.onkeypress(s, "s")
+wn.onkeypress(a, "a")
+wn.onkeypress(d, "d")
+
+wn.listen()
 wn.mainloop()
