8c5312f781e8d7a828db8da0549db220aa4099a5
Matt <Matthew.torres@vtsd.com>
2024-01-18 09:23:21 -0500

    game

--- 1-2-5/game.py
+++ 1-2-5/game.py
@@ -10,18 +10,37 @@ runner.right(180)
 runner.forward(420)
 runner.right(180)
 
+#box setup
+box.penup()
+box.shape("square")
+box.shapesize(1)
+box.goto(400,-50)
+boxxposition = 400
+boxyposition = -50
+
+def win():
+    if runner.xcor == box:
+        print("you win")
+    if runner.ycor() == box:
+        print("you win")
+
 #keypress
 def w():
     runner.forward(20)
+    win()
 
 def s():
     runner.back(20)
+    win()
 
 def a():
     runner.left(45)
+    win()
 
 def d():
     runner.right(45)
+    win()
+
 
 #listen for keypress
 wn.onkeypress(w, "w")
