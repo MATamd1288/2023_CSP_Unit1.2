a32a1b72e63b95716ce80084a425a4df8fad14fd
Matt <Matthew.torres@vtsd.com>
2024-01-31 09:14:18 -0500

    hi

--- 1-2-5/game.py
+++ 1-2-5/game.py
@@ -1,11 +1,12 @@
 import turtle as trtl
+import time
 wn = trtl.Screen()
 runner = trtl.Turtle()
 box = trtl.Turtle()
 wn.bgpic("bg2.gif")
-runner.penup()
 
 #turtle position setup
+runner.penup()
 runner.right(180)
 runner.forward(420)
 runner.right(180)
@@ -13,33 +14,42 @@ runner.right(180)
 #box setup
 box.penup()
 box.shape("square")
-box.shapesize(1)
-box.goto(400,-50)
-boxxposition = 400
-boxyposition = -50
-
-def win():
-    if runner.xcor == box:
-        print("you win")
-    if runner.ycor() == box:
-        print("you win")
+box.shapesize(5)
+box.goto(400, -50)
 
 #keypress
 def w():
     runner.forward(20)
     win()
-
 def s():
     runner.back(20)
     win()
-
 def a():
     runner.left(45)
     win()
-
 def d():
     runner.right(45)
     win()
+
+#Game win event
+def win():
+    if runner.xcor() >= 400:
+        print("you win")
+
+        print("It took", start - end, "seconds!")
+
+    if runner.ycor() >= 20:
+        print("you win")
+        print("It took", start - end, "seconds!")
+
+# Calculate the start time
+start = time.time()
+
+# Code here
+
+# Calculate the end time and time taken
+end = time.time()
+length = start - end
 
 
 #listen for keypress
